35f677566dfdb3b24de6ef5e1136a9d756316f89
sayalirwalzade <sayalirwalzade@gmail.com>
2018-06-22 12:56:35 +0530

    init

---
+++ javascript.js
@@ -0,0 +1,20 @@
+console.log(`ascending order`)
+var i=1,j=2,k=4
+
+if(i>j)
+{
+    console.log(`${i}`)
+}
+else
+{
+    console.log(`${j}`)
+}
+if(j>k)
+{console.log(`${j}`)
+}
+else
+{
+    console.log(`${k}`)
+}
+
+
